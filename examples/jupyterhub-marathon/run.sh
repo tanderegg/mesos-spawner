fd307490baf929bdd945f4a80d39167e29230e54
Tim Anderegg <timothy.anderegg@cfpb.gov>
2018-02-26 14:28:40 -0500

    Adds debug output.

--- examples/jupyterhub-marathon/run.sh
+++ examples/jupyterhub-marathon/run.sh
@@ -1,5 +1,6 @@
 virtualenv -p python3 /tmp/env
 /tmp/env/bin/pip install jupyter jupyterhub
+echo "file://$MESOS_SANDBOX/mesos-spawner-init"
 /tmp/env/bin/pip install mesos_spawner --no-index --find-links "file://$MESOS_SANDBOX/mesos-spawner-init"
 npm install configurable-http-proxy
 /tmp/env/bin/jupyterhub --ip 0.0.0.0 --port $PORT
