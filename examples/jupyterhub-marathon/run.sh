86438549c615236cca6de0be9a9ca250406b2b2f
Tim Anderegg <timothy.anderegg@cfpb.gov>
2018-02-26 14:19:39 -0500

    Fix path to spawner.

--- examples/jupyterhub-marathon/run.sh
+++ examples/jupyterhub-marathon/run.sh
@@ -1,5 +1,5 @@
 virtualenv -p python3 /tmp/env
 /tmp/env/bin/pip install jupyter jupyterhub
-/tmp/env/bin/pip install mesos_spawner --no-index --find-links ../../
+/tmp/env/bin/pip install mesos_spawner --no-index --find-links ../../mesos-spawner-init
 npm install configurable-http-proxy
 /tmp/env/bin/jupyterhub --ip 0.0.0.0 --port $PORT
