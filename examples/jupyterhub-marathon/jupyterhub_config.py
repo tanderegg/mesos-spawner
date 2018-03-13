23f2930443f22f71078842a8327702383ba5aa49
Tim Anderegg <timothy.anderegg@cfpb.gov>
2018-03-13 14:52:57 -0400

    Increase http timeout to 60.

--- examples/jupyterhub-marathon/jupyterhub_config.py
+++ examples/jupyterhub-marathon/jupyterhub_config.py
@@ -461,7 +461,7 @@ c.MesosSpawner.debug = True
 #
 #  Once a server has successfully been spawned, this is the amount of time we
 #  wait before assuming that the server is unable to accept connections.
-#c.Spawner.http_timeout = 30
+c.Spawner.http_timeout = 60
 
 ## The IP address (or hostname) the single-user server should listen on.
 #
