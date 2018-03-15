2119cb8051b8c429232b8eab113a69bdd8e839b5
Tim Anderegg <timothy.anderegg@cfpb.gov>
2018-03-15 10:51:36 -0400

    Add PATH to beaker_install

--- src/mesos_spawner/scheduler.py
+++ src/mesos_spawner/scheduler.py
@@ -148,7 +148,7 @@ class JupyterHubScheduler(Scheduler):
                     "env",
                     "virtualenv -p python3 --system-site-packages /tmp/env",
                     "/tmp/env/bin/pip install -I jupyter jupyterhub beakerx",
-                    "/tmp/env/bin/beakerx install",
+                    "PATH=/tmp/env/bin:$PATH /tmp/env/bin/beakerx install",
                     "/tmp/env/bin/jupyterhub-singleuser --cookie-name=$JPY_COOKIE_NAME --debug -y --ip=0.0.0.0 --port $PORT0 --user {}".format(self.current_request['user'])
                 ]),
                 'user': self.current_request['user'],
